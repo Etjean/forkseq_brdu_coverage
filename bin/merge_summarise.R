c6b0050ffb949c7eaf026728b59e0dd5bcf0252f
Etienne from IBENS <et.jean@outlook.fr>
2020-01-08 17:30:25 +0100

    Initial commit with the nextflow pipeline

---
+++ bin/merge_summarise.R
@@ -0,0 +1,33 @@
+#!/usr/bin/env Rscript
+library(docopt)
+"Usage:
+    merge_summarise.R -o=FILE FILE...
+
+Mandatory arguments:
+    -o FILE, --output FILE      Output file
+    
+" -> doc
+args = docopt(doc)
+file_out=args$output
+files_in=args$FILE
+library(dplyr)
+
+
+# Initialize all_data
+all_data=NULL
+
+# For all files
+for (file in files_in) {
+	# Read data
+    data = readRDS(file)
+    # Merge tables
+    all_data = rbind(all_data, data)
+}
+
+all_data %>%
+# Summarise
+tibble::as_tibble() %>%
+group_by(read_chrm, bin) %>% 
+summarise(cov_nano = sum(cov_nano), cov_brdu=sum(cov_brdu), cov_track=sum(cov_track)) %>%
+# Save data
+saveRDS(file_out)
